0485426c374d09ba703c9e8dfccbd808b0b8da86
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-12-12 17:44:18 +0530

    Updated the flask app for creation of multiple team by the same team leader

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def register():
     password = request.form['password']
     confirm_password = request.form['confirm_password']
     
-    if obj.check_client(email):
+    if obj.check_client({'Email': email}):
         return abort(400, "Email already exist")
     if password == confirm_password:
         username = obj.add_client(email, password, confirm_password)
@@ -45,10 +45,39 @@ def lobby():
     TeamMem_email = request.form.getlist('email')
     TeamMem_profession = request.form.getlist('profession')
     
-    result = obj.get_client(email)
+    result = obj.get_client(email, False)
     if result['User_name'] != TeamLead_username:
+        # update the username if the team lead changed that.
         obj.update_client(email, {"$set": {'User_name': TeamLead_username}})
+    
+    # update the profession of team leader.
     obj.update_client(email, {"$set": {'TeamLead_profession': TeamLead_profession}})
+    
+    if result['team_status'] == True:
+        updated_team_number = result['team_number'] + 1
+        obj.update_client(email, {"$set": {'team_number': updated_team_number}})
+    else:
+        updated_team_number = 0
+        obj.update_client(email, {"$set": {'team_status': True}})
+        obj.update_client(email, {"$set": {'team_number': updated_team_number}})
+    
+    # Updating the creadentials for team members:-
+    count_ls = [i for i in range(1, len(TeamMem_profession) + 1)]
+    iterable_item = zip(count_ls, TeamMem_username, TeamMem_email, TeamMem_profession)
+    team_member_ls = []
+    
+    for counter, username, useremail, profession in iterable_item:
+        member = "TeamMember_" + str(counter)
+        member_details = {member: {"User_name": username, "Email": useremail, "profession": profession}}
+        team_member_ls.append(member_details)
+    
+    updated_team_detail = {str(updated_team_number): {
+        "Team_members_details": team_member_ls, 'Team_name': Team_name}}
+    obj.update_client(email, {"$set": updated_team_detail})
+    
+    # updated_param = {"Team_details": [{"Team_members_details": team_member_ls, 'Team_name': Team_name}]}
+    # obj.update_client(email, {"$set": updated_param})
+    
     return TeamLead_profession
 
 if __name__ == "__main__":
