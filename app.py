2c96faaa634170a4192ca2f3fc47b91aa07843bb
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-12-16 13:07:00 +0530

    Modularized code for main app file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from types import MethodDescriptorType
 from flask import Flask, render_template, request, redirect, abort
-from login_app.login import User, login_check, register_check
+from login_app.login import User, login_check, register_check, lobby_check
 
 app = Flask(__name__)
 
@@ -26,6 +26,7 @@ def register():
     password = request.form['password']
     confirm_password = request.form['confirm_password']
     
+    # register_check performs all of the operations needed for registration.
     return register_check(obj, email, password, confirm_password)
 
 @app.route('/lobby', methods=['POST'])
@@ -39,41 +40,16 @@ def lobby():
     TeamMem_profession = request.form.getlist('profession')
     
     result = obj.get_client(email, False)
-    if result['User_name'] != TeamLead_username:
-        # update the username if the team lead changed that.
-        obj.update_client(email, {"$set": {'User_name': TeamLead_username}})
+    context = {
+        "TeamLead_username": TeamLead_username,
+        "TeamLead_profession": TeamLead_profession,
+        "Team_name": Team_name,
+        "TeamMem_username": TeamMem_username,
+        "TeamMem_email": TeamMem_email,
+        "TeamMem_profession": TeamMem_profession,
+    }
     
-    # update the profession of team leader.
-    obj.update_client(email, {"$set": {'TeamLead_profession': TeamLead_profession}})
-    
-    if result['team_status'] == True:
-        updated_team_number = result['team_number'] + 1
-        obj.update_client(email, {"$set": {'team_number': updated_team_number}})
-    else:
-        updated_team_number = 0
-        obj.update_client(email, {"$set": {'team_status': True}})
-        obj.update_client(email, {"$set": {'team_number': updated_team_number}})
-    
-    # Updating the creadentials for team members:-
-    count_ls = [i for i in range(1, len(TeamMem_profession) + 1)]
-    iterable_item = zip(count_ls, TeamMem_username, TeamMem_email, TeamMem_profession)
-    team_member_dict = {}
-    
-    for counter, username, useremail, profession in iterable_item:
-        member = "TeamMember_" + str(counter)
-        # member_details = {member: {"User_name": username, "Email": useremail, "profession": profession}}
-        team_member_dict[member] = {
-            "User_name": username, "Email": useremail, "profession": profession}
-    
-    updated_team_detail = {str(updated_team_number): {
-        "Team_members_details": team_member_dict, 'Team_name': Team_name}}
-    obj.update_client(email, {"$set": updated_team_detail})
-    
-    # Create another collection for teams
-    obj.team_creation()
-    obj.create_team({Team_name: team_member_dict})
-    
-    return TeamLead_profession
+    return lobby_check(obj, result, email, context)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- login_app/login.py
+++ login_app/login.py
@@ -73,3 +73,51 @@ def register_check(obj, email, password, confirm_password):
         username = obj.add_client(email, password, confirm_password)
         return render_template('index.html', username=username)
     return abort(400, "The Both password does not match")
+
+def lobby_check(obj, result, email, context):
+    TeamLead_username = context.get("TeamLead_username")
+    TeamLead_profession = context.get("TeamLead_profession")
+    Team_name = context.get("Team_name")
+    TeamMem_username = context.get("TeamMem_username")
+    TeamMem_email = context.get("TeamMem_email")
+    TeamMem_profession = context.get("TeamMem_profession")
+    
+    if result['User_name'] != TeamLead_username:
+        # update the username if the team lead changed that.
+        obj.update_client(email, {"$set": {'User_name': TeamLead_username}})
+
+    # update the profession of team leader.
+    obj.update_client(
+        email, {"$set": {'TeamLead_profession': TeamLead_profession}})
+
+    if result['team_status'] == True:
+        updated_team_number = result['team_number'] + 1
+        obj.update_client(
+            email, {"$set": {'team_number': updated_team_number}})
+    else:
+        updated_team_number = 0
+        obj.update_client(email, {"$set": {'team_status': True}})
+        obj.update_client(
+            email, {"$set": {'team_number': updated_team_number}})
+
+    # Updating the creadentials for team members:-
+    count_ls = [i for i in range(1, len(TeamMem_profession) + 1)]
+    iterable_item = zip(count_ls, TeamMem_username,
+                        TeamMem_email, TeamMem_profession)
+    team_member_dict = {}
+
+    for counter, username, useremail, profession in iterable_item:
+        member = "TeamMember_" + str(counter)
+        # member_details = {member: {"User_name": username, "Email": useremail, "profession": profession}}
+        team_member_dict[member] = {
+            "User_name": username, "Email": useremail, "profession": profession}
+
+    updated_team_detail = {str(updated_team_number): {
+        "Team_members_details": team_member_dict, 'Team_name': Team_name}}
+    obj.update_client(email, {"$set": updated_team_detail})
+
+    # Create another collection for teams
+    obj.team_creation()
+    obj.create_team({Team_name: team_member_dict})
+
+    return TeamLead_profession
