aad506d8feb4da8ef89c2b4f937ad03489c37a5f
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-03-02 23:00:40 +0530

    Code added under the route

--- app.py
+++ app.py
@@ -109,10 +109,29 @@ def register():
     # register_check performs all of the operations needed for registration.
     return register_check(obj, email, password, confirm_password)
 
+
 @app.route('/The_lobby', methods=['POST'])
 def lobby():
-    caller = Caller().for_lobby()
-    return caller
+    email = obj.email
+    TeamLead_username = request.form['TeamLead_username']
+    TeamLead_profession = request.form['TeamLead_profession']
+    Team_name = request.form['team_name']
+    TeamMem_username = request.form.getlist('username')
+    TeamMem_email = request.form.getlist('email')
+    TeamMem_profession = request.form.getlist('profession')
+
+    result = obj.get_client(email, False)
+    context = {
+        "TeamLead_username": TeamLead_username,
+        "TeamLead_profession": TeamLead_profession,
+        "Team_name": Team_name,
+        "TeamMem_username": TeamMem_username,
+        "TeamMem_email": TeamMem_email,
+        "TeamMem_profession": TeamMem_profession,
+    }
+
+    return lobby_check(obj, result, email, context)
+
 
 @app.route('/main_lobby', methods=['POST'])
 def main_lobby():
